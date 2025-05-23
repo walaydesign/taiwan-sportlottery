a5142a386dd05784734684788e14d54e3b688019
youzhen <yl301114@gmail.com>
2025-05-23 02:15:56 +0100

    finish

---
+++ public/js/common.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".banner__swiper", {
+    slidesPerView: 1,
+    speed: 1000,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    effect: "creative",
+    creativeEffect: {
+        prev: {
+            shadow: true,
+            translate: [0, 0, -400],
+        },
+        next: {
+            translate: ["100%", 0, 0],
+        },
+    },
+})
